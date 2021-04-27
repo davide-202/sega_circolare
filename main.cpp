90c2afd344760fc387361db8ef66accc82dfaf73
za <davide.boscia@studenti.unitn.it>
2021-04-27 20:28:51 +0200

    aggiunta misure al file svg

--- main.cpp
+++ main.cpp
@@ -81,9 +81,16 @@ string sega_to_svg(sega_circolare* sega){
     s=s+"  <line x1=\""+to_string((sega->lunghezza)/2+5)+"\" y1=\""+to_string(sega->raggio+10)+"\" x2=\""+to_string((sega->lunghezza)/2+5-(sega->raggio/sqrt(2)))+"\" y2=\""+to_string(sega->raggio+10-(sega->raggio/sqrt(2)))+"\" style=\"stroke:grey;stroke-width:4\" />\n";
     s=s+"   <line x1=\""+to_string((sega->lunghezza)/2+5)+"\" y1=\""+to_string(sega->raggio+10)+"\" x2=\""+to_string((sega->lunghezza)/2+5)+"\" y2=\""+to_string(10)+"\" style=\"stroke:grey;stroke-width:4\" />\n";
     s=s+"    <line x1=\""+to_string((sega->lunghezza)/2+5)+"\" y1=\""+to_string(sega->raggio+10)+"\" x2=\""+to_string((sega->lunghezza)/2+5+(sega->raggio/sqrt(2)))+"\" y2=\""+to_string(sega->raggio+10-(sega->raggio/sqrt(2)))+"\" style=\"stroke:grey;stroke-width:4\" />\n";
+    s=s+"   <line x1=\""+to_string(3)+"\" y1=\""+to_string(sega->altezza+sega->raggio+20)+"\" x2=\""+to_string(sega->lunghezza+7)+"\" y2=\""+to_string(sega->altezza+sega->raggio+20)+"\" style=\"stroke:black;stroke-width:2\" />\n";
+    s=s+"  <line x1=\""+to_string(sega->lunghezza+15)+"\" y1=\""+to_string(sega->altezza+sega->raggio+12)+"\" x2=\""+to_string(sega->lunghezza+15)+"\" y2=\""+to_string(8)+"\" style=\"stroke:black;stroke-width:2\" />\n";
+    s=s+" <line x1=\""+to_string(sega->lunghezza+10)+"\" y1=\""+to_string(sega->raggio+12)+"\" x2=\""+to_string(sega->lunghezza+20)+"\" y2=\""+to_string(sega->raggio+12)+"\" style=\"stroke:black;stroke-width:2\" />\n";
+    s=s+"<text x=\""+to_string(sega->lunghezza+20)+"\" y=\""+to_string((sega->raggio+20)/2)+"\" fill=\"black\" >"+to_string(sega->raggio)+"(cm)</text>\n";
+    s=s+"<text x=\""+to_string((sega->lunghezza)/4)+"\" y=\""+to_string(sega->raggio+33+sega->altezza)+"\" fill=\"black\" >"+to_string(sega->lunghezza)+"(cm)</text>\n";
+    s=s+"<text x=\""+to_string(sega->lunghezza+20)+"\" y=\""+to_string(sega->raggio+10+(sega->altezza)/2)+"\" fill=\"black\" >"+to_string(sega->altezza)+"(cm)</text>\n";
     s=s+"</svg>\n";
     return s;
 }
+
 
 void sega_save(string filename, string s){
 
